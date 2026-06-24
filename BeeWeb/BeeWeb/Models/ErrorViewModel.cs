c5537c1b38ab8d5a2c10aacab8f9527f92bcf77d
blaz snk <blasasto0914@gmail.com>
2026-06-23 23:22:48 -0500

    creación del proyecto

---
+++ BeeWeb/BeeWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BeeWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
